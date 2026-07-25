b72796af880447f7b1dceec18c67f144e022dc26
leonardo.valdiviesoclavijo8187@upse.edu.ec <leonardovaldivieso2017@gmail.com>
2026-07-24 21:01:28 -0500

    Agregar GitHub Actions CI/CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+    plugins: [
+        react()
+    ],
+
+    server: {
+
+        proxy: {
+
+            "/api": {
+
+                target: "http://143.244.175.21",
+
+                changeOrigin: true
+
+            }
+
+        }
+
+    }
+
 })
