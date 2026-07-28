58d6ab13040ed9cc7cbefba0026e0c5c34da66cd
leonardo.valdiviesoclavijo8187@upse.edu.ec <leonardovaldivieso2017@gmail.com>
2026-07-28 08:54:39 -0500

    actualizacion de docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
 
-    plugins:[
-        react()
-    ],
+  plugins: [
+    react()
+  ],
 
+  server: {
 
-    server:{
+    host: "0.0.0.0",
 
-        proxy:{
+    proxy: {
 
-            "/api":{
+      "/api": {
 
-                target:"http://localhost:3000",
+        target: "http://backend:3000",
 
-                changeOrigin:true,
+        changeOrigin: true,
 
-                secure:false
+        secure: false
 
-            }
-
-        }
+      }
 
     }
+
+  }
 
 })
